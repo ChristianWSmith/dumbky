6c064e8ad2746a57f75a41db6911d5212fdafa19
Christian Smith <smith.christian.william@gmail.com>
2025-05-22 19:55:19 -0400

    cleaned up browser, might delete later idk ;)

--- internal/ui/views/dashboardsidebarview/dashboardsidebar.go
+++ internal/ui/views/dashboardsidebarview/dashboardsidebar.go
@@ -1,23 +1,37 @@
 package dashboardsidebarview
 
 import (
-	"fmt"
+	"dumbky/internal/log"
 	"os"
 	"path/filepath"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
 type DashboardSidebarView struct {
-	UI *fyne.Container
-	//workingDirectory string
+	UI                      *fyne.Container
+	WorkingDirectoryBinding binding.String
+	SelectedFileBinding     binding.String
+
+	fileList *widget.List
 }
 
 func ComposeDashboardSidebarView() DashboardSidebarView {
 	currentPath, _ := os.Getwd()
-	pathLabel := widget.NewLabel(currentPath)
+
+	workingDirectoryBind := binding.NewString()
+	err := workingDirectoryBind.Set(currentPath)
+	if err != nil {
+		log.Error(err)
+	}
+
+	pathLabel := widget.NewLabel("")
+	pathLabel.Bind(workingDirectoryBind)
+
+	selectedFileBinding := binding.NewString()
 
 	fileList := widget.NewList(
 		func() int { return 0 },
@@ -27,60 +41,94 @@ func ComposeDashboardSidebarView() DashboardSidebarView {
 		func(i widget.ListItemID, o fyne.CanvasObject) {},
 	)
 
-	var updateFileList func(string)
+	ui := container.NewBorder(pathLabel, nil, nil, nil, fileList)
 
-	updateFileList = func(path string) {
-		files, err := os.ReadDir(path)
-		if err != nil {
-			pathLabel.SetText("Failed to read directory")
-			return
+	dsv := DashboardSidebarView{
+		UI:                      ui,
+		WorkingDirectoryBinding: workingDirectoryBind,
+		fileList:                fileList,
+		SelectedFileBinding:     selectedFileBinding,
+	}
+	go dsv.updateFileList()
+
+	return dsv
+}
+
+func (dsv DashboardSidebarView) updateFileList() {
+	workingDirectory, err := dsv.WorkingDirectoryBinding.Get()
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	items, err := collectFileInfo(workingDirectory)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	fyne.Do(func() {
+		dsv.updateFileListWidget(items, workingDirectory)
+	})
+}
+
+func (dsv DashboardSidebarView) updateFileListWidget(items []os.FileInfo, workingDirectory string) {
+	dsv.fileList.Length = func() int {
+		return len(items)
+	}
+	dsv.fileList.UpdateItem = func(i widget.ListItemID, o fyne.CanvasObject) {
+		label := o.(*widget.Label)
+		if i == 0 {
+			label.SetText("..")
+		} else {
+			label.SetText(items[i].Name())
 		}
-
-		items := []os.FileInfo{}
-		items = append(items, nil) // For ".."
-
-		for _, file := range files {
-			info, _ := file.Info()
-			items = append(items, info)
-		}
-
-		fileList.Length = func() int {
-			return len(items)
-		}
-		fileList.UpdateItem = func(i widget.ListItemID, o fyne.CanvasObject) {
-			label := o.(*widget.Label)
-			if i == 0 {
-				label.SetText("..")
-			} else {
-				label.SetText(items[i].Name())
+	}
+	dsv.fileList.OnSelected = func(i widget.ListItemID) {
+		dsv.fileList.UnselectAll()
+		if i == 0 {
+			err := dsv.WorkingDirectoryBinding.Set(filepath.Dir(workingDirectory))
+			if err != nil {
+				log.Error(err)
+				return
 			}
-		}
-		fileList.OnSelected = func(i widget.ListItemID) {
-			fileList.UnselectAll()
-			if i == 0 {
-				newPath := filepath.Dir(path)
-				currentPath = newPath
-				pathLabel.SetText(newPath)
-				updateFileList(newPath)
+			go dsv.updateFileList()
+		} else {
+			selected := items[i]
+			fullPath := filepath.Join(workingDirectory, selected.Name())
+			if selected.IsDir() {
+				err := dsv.WorkingDirectoryBinding.Set(fullPath)
+				if err != nil {
+					log.Error(err)
+					return
+				}
+				go dsv.updateFileList()
 			} else {
-				selected := items[i]
-				fullPath := filepath.Join(path, selected.Name())
-				if selected.IsDir() {
-					currentPath = fullPath
-					pathLabel.SetText(fullPath)
-					updateFileList(fullPath)
-				} else {
-					fmt.Println("Selected file:", fullPath)
+				err := dsv.SelectedFileBinding.Set(fullPath)
+				if err != nil {
+					log.Error(err)
+					return
 				}
 			}
 		}
-		fileList.Refresh()
+	}
+	dsv.fileList.Refresh()
+}
+
+func collectFileInfo(workingDirectory string) ([]os.FileInfo, error) {
+	items := []os.FileInfo{}
+
+	files, err := os.ReadDir(workingDirectory)
+	if err != nil {
+		log.Error(err)
+		return items, err
 	}
 
-	updateFileList(currentPath)
+	items = append(items, nil) // For ".."
 
-	ui := container.NewBorder(pathLabel, nil, nil, nil, fileList)
-	return DashboardSidebarView{
-		UI: ui,
+	for _, file := range files {
+		info, _ := file.Info()
+		items = append(items, info)
 	}
+	return items, nil
 }
